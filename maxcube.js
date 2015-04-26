d7d4c789aae930d129a23026e0d282470fbf9603
Jakob-Gliwa <jgliwa2@googlemail.com>
2015-04-26 18:35:53 +0200

    Update maxcube.js

--- maxcube.js
+++ maxcube.js
@@ -369,7 +369,7 @@ function setTemperature (rfAdress, mode, temperature, untilDate) {
     modeBin = '10';
     var momentDate = moment(untilDate);
     var year_until = ('0000000' + (momentDate.get('year') - 2000).toString(2)).substr(-7);
-    var month_until = ('0000' + (momentDate.get('month')+1).toString(2)).substr(-4);
+    var month_until = ('0000' + (momentDate.get('month')).toString(2)).substr(-4);
     var day_until = ('00000' + momentDate.get('date').toString(2)).substr(-5);
     date_until = parseInt(('0000' + (month_until.substr(0,3) + day_until + month_until.substr(-1) + year_until)),2).toString(16).substr(-4);
     time_until = ('00' + Math.round((momentDate.get('hour') + (momentDate.get('minute') / 60)) * 2).toString(16)).substr(-2);
